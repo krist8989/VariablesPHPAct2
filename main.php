3125b448e663c6bf69a91fc1a9b847cd1ab57cd9
krist8989 <kristjareno3@gmail.com>
2025-08-13 02:14:39 +0000

    main php syntax

---
+++ main.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$name = "Krist Raymund Jareno";
+$age = 20;
+$school = "Global reciprocal college";
+$course = "Information Technology";
+$subject = "Web Development";
+
+echo "Name: " . $name . "<br>";
+echo "Age: " . $age . "<br>";
+echo "School: " . $school . "<br>";
+echo "Course: " . $course . "<br>";
+echo "Subject: " . $subject . "<br>";
+?>
+
+</body>
+</html>
