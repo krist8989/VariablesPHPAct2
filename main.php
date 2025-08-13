000a0c67585e157ce8bf807cfae291ee217c917b
krist8989 <kristjareno3@gmail.com>
2025-08-13 02:16:26 +0000

    index php syntax

--- main.php
+++ main.php
@@ -8,12 +8,14 @@ $age = 20;
 $school = "Global reciprocal college";
 $course = "Information Technology";
 $subject = "Web Development";
+$student = "yes"; 
 
 echo "Name: " . $name . "<br>";
 echo "Age: " . $age . "<br>";
 echo "School: " . $school . "<br>";
 echo "Course: " . $course . "<br>";
 echo "Subject: " . $subject . "<br>";
+echo "Student: " . ($student ? "Yes" : "No") . "<br>";
 ?>
 
 </body>
